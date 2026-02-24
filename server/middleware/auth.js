06abc235377357ea9a2b4ca7d898829113dfb2a9
Agila-learning <agilaagi0922@gmail.com>
2026-02-24 16:52:52 +0530

    fix: populate clientId in auth middleware for correct HR visibility

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,7 +7,7 @@ const auth = async (req, res, next) => {
         if (!token) throw new Error();
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findOne({ _id: decoded.id });
+        const user = await User.findOne({ _id: decoded.id }).populate('clientId');
 
         if (!user) throw new Error();
 
